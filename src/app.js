f04ee9e10a37b91a341b5140f6e7609628e8d1ce
ebenKB <eakbo23@gmail.com>
2018-12-24 05:09:11 +0000

    remove bug from index markup add scroll animation

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
+alert("hey")
+
 // humburger logic
 document.getElementById('media-mob').addEventListener('click', () => {
   const burgerTop = document.getElementById('burger-top');
   const burgerMid = document.getElementById('burger-mid');
   const burgerDown = document.getElementById('burger-down');
   const mobileView = document.getElementById('mobile-container');
-  // const wrapper    = document.getElementById("wrapper");
-  // const menu 			=  document.getElementById("mobile-menu");
-
 
   burgerTop.classList.toggle('burger-top-rotate');
   burgerDown.classList.toggle('burger-down-rotate');
@@ -17,17 +16,10 @@ document.getElementById('media-mob').addEventListener('click', () => {
   if (burgerTop.classList.contains('burger-top-rotate')) {
     burgerDown.classList.add('green');
     burgerTop.classList.add('green');
-    // add listener to disable scroll
 
-    // menu.classList.add("green");
-    // wrapper.classList.add("hide");
   } else {
     burgerDown.style.backgroundColor = '#fff';
     burgerTop.style.backgroundColor = '#fff';
 
-    // Remove listener to disable scroll
-
-    // wrapper.classList.remove("hide");
-    // menu.classList.remove("green");
   }
 });
