c5534acde61ad78800976eca70a1ffddeb421456
Ebenezer K.B Adjei <ekb.adjei@yahoo.com>
2018-12-26 06:04:32 +0000

    website body (#3)
    * remove bug from index markup
    
    * improve website body
    
    *  improve website body
       improve static header
    
    * replace icon placeholders with actual icons

--- src/app.js
+++ src/app.js
@@ -4,9 +4,6 @@ document.getElementById('media-mob').addEventListener('click', () => {
   const burgerMid = document.getElementById('burger-mid');
   const burgerDown = document.getElementById('burger-down');
   const mobileView = document.getElementById('mobile-container');
-  // const wrapper    = document.getElementById("wrapper");
-  // const menu 			=  document.getElementById("mobile-menu");
-
 
   burgerTop.classList.toggle('burger-top-rotate');
   burgerDown.classList.toggle('burger-down-rotate');
@@ -17,17 +14,10 @@ document.getElementById('media-mob').addEventListener('click', () => {
   if (burgerTop.classList.contains('burger-top-rotate')) {
     burgerDown.classList.add('green');
     burgerTop.classList.add('green');
-    // add listener to disable scroll
 
-    // menu.classList.add("green");
-    // wrapper.classList.add("hide");
   } else {
     burgerDown.style.backgroundColor = '#fff';
     burgerTop.style.backgroundColor = '#fff';
 
-    // Remove listener to disable scroll
-
-    // wrapper.classList.remove("hide");
-    // menu.classList.remove("green");
   }
 });
