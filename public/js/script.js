404760e7765ec26bf6c1a752564a80b800b3ebc4
ebenKB <eakbo23@gmail.com>
2019-01-18 16:29:36 +0000

    generate site

---
+++ public/js/script.js
@@ -0,0 +1,28 @@
+// humburger logic
+document.getElementById('media-mob').addEventListener('click', () => {
+  const burgerTop = document.getElementById('burger-top');
+  const burgerMid = document.getElementById('burger-mid');
+  const burgerDown = document.getElementById('burger-down');
+  const mobileView = document.getElementById('mobile-container');
+
+  burgerTop.classList.toggle('burger-top-rotate');
+  burgerDown.classList.toggle('burger-down-rotate');
+
+  burgerMid.classList.toggle('hide');
+  mobileView.classList.toggle('hide');
+
+  if (burgerTop.classList.contains('burger-top-rotate')) {
+    burgerDown.classList.add('green');
+    burgerTop.classList.add('green');
+
+  } else {
+    burgerDown.style.backgroundColor = '#fff';
+    burgerTop.style.backgroundColor = '#fff';
+
+  }
+});
+
+window.addEventListener('scroll', function(e) {
+  console.log(e);
+  document.getElementById('contact-header').classList.add('hide')
+})
