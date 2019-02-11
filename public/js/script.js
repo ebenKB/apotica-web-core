2e77d35e7f38d0e046e3829fe50b21616589f5e6
ebenKB <eakbo23@gmail.com>
2019-02-11 12:46:44 +0000

    generate site

--- public/js/script.js
+++ public/js/script.js
@@ -127,3 +127,34 @@ for (i = 0; i < acc.length; i++) {
 
  });
 }
+
+//adding scroll - animates scrolling
+
+function animate(elem, style, unit, from, to, time, prop) {
+  if (!elem) {
+      return;
+  }
+
+  let start = new Date().getTime(),
+      timer = setInterval(function () {
+          let step = Math.min(1, (new Date().getTime() - start) / time);
+          if (prop) {
+              elem[style] = (from + step * (to - from))+unit;
+          } else {
+              elem.style[style] = (from + step * (to - from))+unit;
+          }
+          if (step === 1) {
+              clearInterval(timer);
+          }
+      }, 25);
+  if (prop) {
+        elem[style] = from+unit;
+  } else {
+        elem.style[style] = from+unit;
+  }
+}
+
+document.getElementById('to-media-mob').addEventListener('click', () =>{
+  let target = document.getElementById("media-probe");
+  animate(document.scrollingElement || document.documentElement, "scrollTop", "", 0, target.offsetTop, 2000, true);
+});
