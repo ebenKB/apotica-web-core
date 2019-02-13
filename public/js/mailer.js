babc5ecb59d86b26be41bfd91aca65b3686251f0
ebenKB <eakbo23@gmail.com>
2019-02-13 09:38:53 +0000

    generate site

--- public/js/mailer.js
+++ public/js/mailer.js
@@ -1,12 +1,12 @@
-Email.send({
-  Host : "smtp.office365.com",
-  Username : "relay@apotica.net",
-  Password : "Apotica@123",
-  To : 'eakbo23@gmail.com',
-  Username : "relay@apotica.net",
-  From : "relay@apotica.net",
-  Subject : "This is the subject",
-  Body : "And this is the body"
-}).then(
-message => alert(message)
-);
+// Email.send({
+//   Host : "smtp.office365.com",
+//   Username : "relay@apotica.net",
+//   Password : "Apotica@123",
+//   To : 'eakbo23@gmail.com',
+//   Username : "relay@apotica.net",
+//   From : "relay@apotica.net",
+//   Subject : "This is the subject",
+//   Body : "And this is the body"
+// }).then(
+// message => alert(message)
+// );
