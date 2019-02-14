54ca55e6bd9414a9600caf37e722bf6a0351887c
ebenKB <eakbo23@gmail.com>
2019-02-14 14:29:16 +0000

    a little change

--- themes/apotica-theme/source/js/mailer.js
+++ themes/apotica-theme/source/js/mailer.js
@@ -1,12 +1,17 @@
-// Email.send({
-//   Host : "smtp.office365.com",
-//   Username : "relay@apotica.net",
-//   Password : "Apotica@123",
-//   To : 'eakbo23@gmail.com',
-//   Username : "relay@apotica.net",
-//   From : "relay@apotica.net",
-//   Subject : "This is the subject",
-//   Body : "And this is the body"
-// }).then(
-// message => alert(message)
-// );
+Email.send({
+
+  UseDefaultCredentials: false,
+  Host : "smtp.office365.com",
+  ssl:true,
+  tls:true,
+  port:587,
+  Username : "relay@apotica.net",
+  Password : "Apotica@123",
+  To : 'web@apotica.net',
+  Username : "relay@apotica.net",
+  From : "relay@apotica.net",
+  Subject : "This is the subject",
+  Body : "And this is the body"
+}).then(
+message => alert(message)
+);
