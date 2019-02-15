fb8dede623a2f1f8ac46c46ef5a2397c1fa6a5bc
ebenKB <eakbo23@gmail.com>
2019-02-15 09:54:50 +0000

    disable mail script

--- themes/apotica-theme/source/js/mailer.js
+++ themes/apotica-theme/source/js/mailer.js
@@ -1,17 +1,17 @@
-Email.send({
+// Email.send({
 
-  UseDefaultCredentials: false,
-  Host : "smtp.office365.com",
-  ssl:true,
-  tls:true,
-  port:587,
-  Username : "relay@apotica.net",
-  Password : "Apotica@123",
-  To : 'web@apotica.net',
-  Username : "relay@apotica.net",
-  From : "relay@apotica.net",
-  Subject : "This is the subject",
-  Body : "And this is the body"
-}).then(
-message => alert(message)
-);
+//   UseDefaultCredentials: false,
+//   Host : "smtp.office365.com",
+//   ssl:true,
+//   tls:true,
+//   port:587,
+//   Username : "relay@apotica.net",
+//   Password : "Apotica@123",
+//   To : 'web@apotica.net',
+//   Username : "relay@apotica.net",
+//   From : "relay@apotica.net",
+//   Subject : "This is the subject",
+//   Body : "And this is the body"
+// }).then(
+// message => alert(message)
+// );
