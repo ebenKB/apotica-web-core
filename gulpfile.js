ade41aac6cdfa000ba433b58ce916affe48b5f20
ebenKB <eakbo23@gmail.com>
2019-03-05 20:01:40 +0000

    improve mobile view for main hero links add campaign page add UI fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,18 @@
 const gulp      = require('gulp');
-const babel     = require('gulp-babel');
-const connect   = require('gulp-connect');
+var postcss      = require('gulp-postcss');
+var sourcemaps   = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
 
-gulp.task('js', () =>
-	gulp.src('src/app.js')
-		.pipe(babel({
-			presets: ['@babel/env']
-		}))
-		.pipe(gulp.dest('dist/assets/js'))
-);
-
-
-gulp.task('connect', function() {
-  connect.server({
-    root: 'dist',
-    livereload: true
-  });
+gulp.task('autoprefixer', function () {
+  return gulp.src('./themes/apotica-theme/source/_css/**/*.css')
+  .pipe(sourcemaps.init())
+  .pipe(postcss([ autoprefixer() ]))
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('./themes/apotica-theme/source/css'));
 });
-
-gulp.task('css', function () {
-    gulp.src('./assets/*.css')
-      .pipe(gulp.dest('./dist/assets/css'))
-      .pipe(connect.reload());
-});
-
-gulp.task('images', function () {
-    gulp.src('./assets/images/*.*')
-      .pipe(gulp.dest('./dist/assets/images'))
-      .pipe(connect.reload());
-});
-
-
-gulp.task('html', function () {
-  gulp.src('./src/*.html')
-    .pipe(gulp.dest('./dist'))
-    .pipe(connect.reload());
-});
-
-gulp.task('video',function(){
-  gulp.src('./assets/video/*.mp4')
-  .pipe(gulp.dest('./dist/assets/video'))
-  .pipe(connect.reload());
-});
-
-gulp.task('font',function(){
-  gulp.src('./assets/fonts/*.*')
-  .pipe(gulp.dest('./dist/assets/fonts'))
-  .pipe(connect.reload());
-})
 
 gulp.task('watch', function () {
-    gulp.watch(['./src/*.html'], ['html']);
-    gulp.watch(['./assets/*.css'], ['css']);
+    gulp.watch(['./themes/apotica-theme/source/_css/**/*.css'], ['css']);
 });
 
-
-gulp.task('default', ['connect','css', 'video','font', 'js','images', 'html', 'watch']);
+gulp.task( ['autoprefixer', 'watch']);
