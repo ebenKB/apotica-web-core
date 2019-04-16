9f28f188986304d177ebe00bfd2efcb8faf5735d
ebenKB <eakbo23@gmail.com>
2019-04-16 16:55:48 +0000

    improve app-dynamics

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,50 @@
 const gulp      = require('gulp');
 var postcss      = require('gulp-postcss');
-var sourcemaps   = require('gulp-sourcemaps');
-var autoprefixer = require('autoprefixer');
+const sourcemaps   = require('gulp-sourcemaps');
+const autoprefixer = require('autoprefixer');
+const { series, parallel } = require('gulp');
+// const uglify = require ('gulp-uglify');
 
-gulp.task('autoprefixer', function () {
-  return gulp.src('./themes/apotica-theme/source/_css/**/*.css')
-  .pipe(sourcemaps.init())
-  .pipe(postcss([ autoprefixer() ]))
-  .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest('./themes/apotica-theme/source/css'));
-});
+// const gulp      = require('gulp');
+// var postcss      = require('gulp-postcss');
+// var sourcemaps   = require('gulp-sourcemaps');
+// var autoprefixer = require('autoprefixer');
+
+// gulp.task('autoprefixer', function () {
+//   return gulp.src('./themes/apotica-theme/source/_css/**/*.css')
+//   .pipe(sourcemaps.init())
+//   .pipe(postcss([ autoprefixer() ]))
+//   .pipe(sourcemaps.write('.'))
+//   .pipe(gulp.dest('./themes/apotica-theme/source/css'));
+// });
 
 // gulp.task('js', function() {
 //   return gulp.src('./themes/apotica-theme/source/_css/**/*.css')
 // })
 
-gulp.task('watch', function () {
-    gulp.watch(['./themes/apotica-theme/source/_css/**/*.css'], ['css']);
-});
+// gulp.task('watch', function () {
+//     gulp.watch(['./themes/apotica-theme/source/_css/**/*.css'], ['css']);
+// });
 
-gulp.task(['autoprefixer', 'watch']);
+function clean(cb){
+  cb();
+}
+
+function cssMinify(cb) {
+  cb();
+}
+
+function autoprefix(cb) {
+  gulp.src('./themes/apotica-theme/source/_css/**/*.css')
+  .pipe(sourcemaps.init())
+  .pipe(postcss([ autoprefixer() ]))
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('./themes/apotica-theme/source/css'));
+  cb();
+}
+
+gulp.watch('./themes/apotica-theme/source/_css/**/*.css', autoprefix, function(cb) {
+  cb();
+})
+
+exports.build = series(clean, parallel(autoprefix, cssMinify));
